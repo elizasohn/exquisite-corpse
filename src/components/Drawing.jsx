cc82497727e3e7716a89c8112ea98c629c7bf19f
Eliza Sohn <eliza.sohn@gmail.com>
2020-02-28 13:11:34 -0800

    create initial template

---
+++ src/components/Drawing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DrawingLine from './DrawingLine';
+
+function Drawing({ lines }) {
+  return (
+    <svg className="drawing">
+      {lines.map((line, index) => (
+        <DrawingLine key={index} line={line} />
+      ))}
+    </svg>
+  );
+}
+
+export default Drawing;
---
+++ src/components/DrawingLine.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function DrawingLine({ line }) {
+  const pathData = "M " +
+    line
+      .map(p => {
+        return `${p.get('x')} ${p.get('y')}`;
+      })
+      .join(" L ");
+
+  return <path className="path" d={pathData} />;
+}
+
+export default DrawingLine;
